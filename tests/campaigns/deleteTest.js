7ef89add8782bdee841b2923d4a31a2957a7be57
actodi9239 <diegoacosta9239@gmail.com>
2024-10-16 17:10:28 -0400

    update test case of delete campaigns

--- tests/campaigns/deleteTest.js
+++ tests/campaigns/deleteTest.js
@@ -1,43 +1,52 @@
-const { expect } = require('chai');
-const tags = require('mocha-tags');
+const { expect } = require("chai");
+const tags = require("mocha-tags");
 
-const NavbarPage = require('../../main/pages/navbar/navbarPage');
-const NavbarMorePage = require('../../main/pages/navbar/navbarMorePage');
-const ListCampaingPage = require('../../main/pages/campaigns/listCampaignPage');
-const ModalPage = require('../../main/pages/base/modalPage');
-const ViewCampaignPage = require('../../main/pages/campaigns/viewCampaignPage');
-const testConfig = require('../../testConfig.json');
-const LoginPage = require('../../main/pages/loginPage');
-const { myBefore, myAfter, myAfterScreen } = require('../../main/hooks');
-const { create } = require('../../main/api/campaigns/serviceCampaings');
-const data = require('../../utils/campaigns/dataDefault.json');
+const NavbarPage = require("../../main/pages/navbar/navbarPage");
+const NavbarMorePage = require("../../main/pages/navbar/navbarMorePage");
+const ListCampaingPage = require("../../main/pages/campaigns/listCampaignPage");
+const ModalPage = require("../../main/pages/base/modalPage");
+const ViewCampaignPage = require("../../main/pages/campaigns/viewCampaignPage");
+const testConfig = require("../../testConfig.json");
+const LoginPage = require("../../main/pages/loginPage");
+const { myBefore, myAfter, myAfterScreen } = require("../../main/hooks");
+const { create } = require("../../main/api/campaigns/serviceCampaings");
+const data = require("../../utils/campaigns/dataDefault.json");
 
-describe('Create Campaing Test', function () {
-    this.timeout(60000);
-    let campaign = '';
-    before(async () => {
-        campaign = await create(data);
-        await myBefore();
-        await LoginPage.isVisible();
-        await LoginPage.setCredentials(testConfig.credentials.username, testConfig.credentials.password)
-        await LoginPage.clickLoginButton();
-        await NavbarPage.clickIconMore();
-        await NavbarMorePage.clickMenuCampaign();
-    });
+describe("Create Campaing Test", function () {
+  this.timeout(60000);
+  let campaign = "";
+  before(async () => {
+    campaign = await create(data);
+    await myBefore();
+    await LoginPage.isVisible();
+    await LoginPage.setCredentials(
+      testConfig.credentials.username,
+      testConfig.credentials.password
+    );
+    await LoginPage.clickLoginButton();
 
-    afterEach(async function () {
-        await myAfterScreen.call(this);
-    });
+    await NavbarPage.isVisible();
+    await NavbarPage.clickIconMore();
 
-    after(async () => {
-        await myAfter();
-    });
+    await NavbarMorePage.isVisible();
+    await NavbarMorePage.clickMenuCampaign();
+  });
 
-    it('Verify delete campaign with page view', async () => {
-        await ListCampaingPage.clickCampaignRow(campaign.id);
-        await ViewCampaignPage.clickMoreButton()
-        await ViewCampaignPage.clickDeleteButton()
-        await ModalPage.clickConfirmButton()
-        expect(await ListCampaingPage.getColumnTextsName()).to.not.include(campaign.name);
-    })
+  afterEach(async function () {
+    await myAfterScreen.call(this);
+  });
+
+  after(async () => {
+    await myAfter();
+  });
+
+  it("Verify delete campaign with page view", async () => {
+    await ListCampaingPage.clickCampaignRow(campaign.id);
+    await ViewCampaignPage.clickMoreButton();
+    await ViewCampaignPage.clickDeleteButton();
+    await ModalPage.clickConfirmButton();
+    expect(await ListCampaingPage.getColumnTextsName()).to.not.include(
+      campaign.name
+    );
+  });
 });
