6818cdaa1de4bf74eeba08011ff83d62b803cd0c
actodi9239 <diegoacosta9239@gmail.com>
2024-10-17 10:44:19 -0400

    test create campaings in ci

--- core/browser/remoteDriver.js
+++ core/browser/remoteDriver.js
@@ -18,7 +18,7 @@ module.exports = class RemoteDriver {
       myLogger.info(`Creating Driver with configuration: ${JSON.stringify(capabilities, null, 2)}`)
 
       return await new Builder().
-        usingServer("https://diegoplacidoacos_wBGDaP:MyvVtM6uBFDsH8BdqEdV@hub-cloud.browserstack.com/wd/hub").
+        usingServer("https://testing_MtXUbk:4CWLK6Wi4HAQjPJ5gAc3@hub-cloud.browserstack.com/wd/hub").
         withCapabilities(capabilities).
         build();
     })();
