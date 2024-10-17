802d212aff883319da362eefab6f268aa3896a7b
actodi9239 <diegoacosta9239@gmail.com>
2024-10-17 16:58:44 -0400

    add correction test case of list  target list

--- tests/targetList/indexTest.js
+++ tests/targetList/indexTest.js
@@ -1,45 +1,55 @@
-const { expect } = require('chai');
-const tags = require('mocha-tags');
+const { expect } = require("chai");
+const tags = require("mocha-tags");
 
-const NavbarPage = require('../../main/pages/navbar/navbarPage');
-const NavbarMorePage = require('../../main/pages/navbar/navbarMorePage');
-const ListTargetListPage = require('../../main/pages/targetList/listTargetListPage');
-const testConfig = require('../../testConfig.json');
-const LoginPage = require('../../main/pages/loginPage');
-const { myBefore, myAfter, myAfterScreen } = require('../../main/hooks');
+const NavbarPage = require("../../main/pages/navbar/navbarPage");
+const NavbarMorePage = require("../../main/pages/navbar/navbarMorePage");
+const ListTargetListPage = require("../../main/pages/targetList/listTargetListPage");
+const testConfig = require("../../testConfig.json");
+const LoginPage = require("../../main/pages/loginPage");
+const { myBefore, myAfter, myAfterScreen } = require("../../main/hooks");
 
-describe('Create Campaing Test', function () {
-    this.timeout(50000);
-    before(async () => {
-        await myBefore();
-        await LoginPage.isVisible();
-        await LoginPage.setCredentials(testConfig.credentials.username, testConfig.credentials.password)
-        await LoginPage.clickLoginButton();
+describe("Create Campaing Test", function () {
+  this.timeout(50000);
+  before(async () => {
+    await myBefore();
+    await LoginPage.isVisible();
+    await LoginPage.setCredentials(
+      testConfig.credentials.username,
+      testConfig.credentials.password
+    );
+    await LoginPage.clickLoginButton();
 
-        await NavbarPage.clickIconMore();
-        await NavbarMorePage.clickMenuTargetList();
-    });
+    await NavbarPage.isVisible();
+    await NavbarPage.clickIconMore();
 
-    afterEach(async function () {
-        await myAfterScreen.call(this);
-    });
+    await NavbarMorePage.isVisible();
+    await NavbarMorePage.clickMenuTargetList();
+  });
 
-    after(async () => {
-        await myAfter();
-    });
+  afterEach(async function () {
+    await myAfterScreen.call(this);
+  });
 
-    it('Verify asceding colunm name', async () => {
-        await ListTargetListPage.clickNameTitle();
+  after(async () => {
+    await myAfter();
+  });
 
-        const columnTexts = await ListTargetListPage.getColumnTextsName();
-        const sortedTexts = [...columnTexts].sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
-        expect(columnTexts).to.deep.equal(sortedTexts);
-    })
+  it("Verify ascending column name", async () => {
+    await ListTargetListPage.clickNameTitle();
 
-    it('Verify desceding colunm name', async () => {
-        await ListTargetListPage.clickNameTitle();
-        const columnTexts = await ListTargetListPage.getColumnTextsName();
-        const sortedTextsDesc = [...columnTexts].sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' })).reverse();
-        expect(columnTexts).to.deep.equal(sortedTextsDesc);
-    })
+    const columnTexts = await ListTargetListPage.getColumnTextsName();
+    const sortedTexts = [...columnTexts].sort((a, b) =>
+      a.localeCompare(b, undefined, { sensitivity: "base" })
+    );
+    expect(columnTexts).to.deep.equal(sortedTexts);
+  });
+
+  it("Verify descending column name", async () => {
+    await ListTargetListPage.clickNameTitle();
+    const columnTexts = await ListTargetListPage.getColumnTextsName();
+    const sortedTextsDesc = [...columnTexts]
+      .sort((a, b) => a.localeCompare(b, undefined, { sensitivity: "base" }))
+      .reverse();
+    expect(columnTexts).to.deep.equal(sortedTextsDesc);
+  });
 });
